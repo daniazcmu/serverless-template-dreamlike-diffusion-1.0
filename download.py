6da32614176e0a9566087f84d151a59d357e4932
daniazcmu <127138640+daniazcmu@users.noreply.github.com>
2023-03-10 12:51:59 +0100

    Update download.py

--- download.py
+++ download.py
@@ -3,14 +3,14 @@
 import os
 import time
 import torch
-from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
+from diffusers import StableDiffusionPipeline, StableDiffusionKDiffusionPipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights at build time
     t1 = time.time()
     model_id = "dreamlike-art/dreamlike-diffusion-1.0"
-    model = StableDiffusionPipeline.from_pretrained(
-        model_id
+    model = StableDiffusionKDiffusionPipeline.from_pretrained(
+        model_id, torch_dtype=torch.float16
     )
     t2 = time.time()
     print("Download took - ",t2-t1,"seconds")
